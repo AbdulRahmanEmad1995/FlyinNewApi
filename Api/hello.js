d2b7945a8eea332dadb2f65311608369e104f321
abdo <abdoalmasery223@gmail.com>
2025-05-17 14:22:55 +0300

    sss

--- Api/hello.js
+++ Api/hello.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
     const delay = parseInt(req.query.delay) || 0;
     setTimeout(() => {
+      //ss
       res.status(200).json({ message: `Response delayed by ${delay} ms` });
     }, delay);
   }
